67c69bdc23107ef67d01a6cd7121d9d682f5c5fa
Joshua Daley <43053801+joshua-d@users.noreply.github.com>
2020-02-07 15:37:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var Alexa = require("alexa-sdk");
 var http = require('http');
 
 var date = new Date();
-var day = date.getDay();
+var day = (date.getDay() + 6) % 7;
 
-function httpGet(callback, diningHallURL) {
+
+function get_web_data(callback, diningHallURL) {
+  
     var options = {
         host: 'menus.sodexomyway.com',
         path: diningHallURL,
@@ -15,68 +17,52 @@ function httpGet(callback, diningHallURL) {
 
     var req = http.request(options, res => {
         res.setEncoding('utf8');
-        var responseString = "";
+        var res_data = "";
         
         res.on('data', chunk => {
-            responseString = responseString + chunk;
+            res_data = res_data + chunk;
         });
         
         res.on('end', () => {
-            callback(responseString);
+            callback(res_data);
         });
 
     });
-    req.end();
-}
-
-
-function readResponse(responseString, meal) {
-	
-	var foods = [];
-	
-	var meals = ["BREAKFAST", "LUNCH", "DINNER", "CONTINUOUS", "BRUNCH", "DINNER"];
-	
-	
-	if (day > 4 && meal == 4) {
-	  
-	  for (var i = 0; i < 7 - day; i++) {
-	
-		  responseString = responseString.slice(responseString.indexOf(meals[meal]) + 1, responseString.length);
-	
-	  }
-	  
-	}
-	
-	else {
-	
-  	for (var i = 0; i < day; i++) {
-	
-	  	responseString = responseString.slice(responseString.indexOf(meals[meal]) + 1, responseString.length);
-	
-	  }
-	
-	}
-	
-  
-	responseString = responseString.slice(0, responseString.indexOf(meals[meal + 1]));
-	
-	while (responseString.indexOf("foodItemName") != -1) {
-		
-		responseString = responseString.slice(responseString.indexOf("foodItemName") + 1, responseString.length);
-		
-		foods[foods.length] = responseString.slice(responseString.indexOf("\"") + 1, responseString.indexOf("\" data"));
-		
-	}
-	
-	return foods;
-	
-}
-
-
-var handlers = {
-  "MainIntent": function () {
     
-    var foods;
+    req.end();
+    
+}
+
+
+function readResponse(res_data, mealID) {
+    
+    var foods = [];
+    var meals = ["BREAKFAST", "LUNCH", "AFTERNOON SNACK", "DINNER", "BRUNCH"];
+    var meal = meals[mealID];
+    
+    if (day > 4) {
+      for (var i = 0; i < day - 4; i++)
+        res_data = res_data.slice(res_data.indexOf(meal) + 1);
+    }
+    else
+      for (var i = 0; i < day + 1; i++)
+        res_data = res_data.slice(res_data.indexOf(meal) + 1);
+    
+    res_data = res_data.slice(0, res_data.indexOf("accordion-block"));
+    
+    while (res_data.indexOf("foodItemName") >= 0) {
+        res_data = res_data.slice(res_data.indexOf("foodItemName") + 14);
+        foods[foods.length] = res_data.slice(0, res_data.indexOf("\""));
+    }
+    
+    return foods;
+    
+}
+
+  
+var handlers = {
+  
+  "MainIntent": function () {
     
     var diningHall = this.event.request.intent.slots.diningHall.value;
     var diningHallURL;
@@ -95,57 +81,38 @@ var handlers = {
     switch (diningHall) {
       
       case "C I W":
-        diningHallURL = '/BiteMenu/Menu?menuId=1203&locationId=74015005&whereami=https://binghamton.sodexomyway.com/dining-near-me/woods-dinig-center';
+        diningHallURL = '/BiteMenu/Menu?menuId=17395&locationId=74015005&whereami=https://binghamton.sodexomyway.com/dining-near-me/woods-dinig-center';
         break;
       case "c4":
-        diningHallURL = '/BiteMenu/Menu?menuId=1204&locationId=74015006&whereami=https://binghamton.sodexomyway.com/dining-near-me/chenango-champlain-collegiate-center';
+        diningHallURL = '/BiteMenu/Menu?menuId=18060&locationId=74015006&whereami=https://binghamton.sodexomyway.com/dining-near-me/chenango-champlain-collegiate-center';
         break;
       case "Hinman":
         diningHallURL = '/BiteMenu/Menu?menuId=1205&locationId=74015007&whereami=https://binghamton.sodexomyway.com/dining-near-me/hinman-dining-center';
         break;
       case "Appalachian":
-        diningHallURL = '/BiteMenu/Menu?menuId=1202&locationId=74015009&whereami=https://binghamton.sodexomyway.com/dining-near-me/appalachian-collegiate-center';
+        diningHallURL = '/BiteMenu/Menu?menuId=17338&locationId=74015009&whereami=https://binghamton.sodexomyway.com/dining-near-me/appalachian-collegiate-center';
       
     }
       
     
     var meal = this.event.request.intent.slots.meal.value;
-    var mealID;
-    
-    
-    switch (meal) {
-      
-      case "breakfast":
-        mealID = 0;
-        break;
-      case "lunch":
-        mealID = 1;
-        break;
-      case "dinner":
-        mealID = 2;
-        break;
-      case "brunch":
-        mealID = 4;
-        break;
-      
-    }
+    var meals = ['breakfast', 'lunch', 'afternoon snack', 'dinner', 'brunch'];
+    var mealID = meals.indexOf(meal);
     
     if (day > 4 && mealID < 2) {
-      
       mealID = 4;
       meal = "brunch";
-      
     }
+    
         
-    httpGet(responseString => {
+    get_web_data(res_data => {
 			
-			foods = readResponse(responseString, mealID);
+        var foods = readResponse(res_data, mealID);
+        this.response.speak("For " + meal + ", at " + diningHall + ", there is: " + foods);
+	
+        this.emit(':responseReady');
 			
-			this.response.speak("For " + meal + ", at " + diningHall + ", there is: " + foods);
-		
-		  this.emit(':responseReady');
-			
-		}, diningHallURL);
+    }, diningHallURL);
 		
   },
   
